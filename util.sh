52c6c261f3ba536c12bee859a36dd50515292515
Ganesh Mahendran <opensource.ganesh@gmail.com>
2017-03-24 09:42:15 +0800

    add adb root
    N/A
    
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>

--- util.sh
+++ util.sh
@@ -6,6 +6,7 @@ reboot_device()
 	adb $adb_on_device shell reboot
 	sleep 60 # 1 min
 
+	adb $adb_on_device root
 	echo "  unlock screen"
 	adb $adb_on_device shell input keyevent 26 # press power
 	adb $adb_on_device shell input swipe 400 600 400 200 # swipe up to unlock screen

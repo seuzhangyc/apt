41937061e09e89d7041d8469982745d4235e4b2e
Cai Liu <liucai@xiaomi.com>
2017-03-27 17:44:29 +0800

    add sleep after adb root
    N/A
    
    Signed-off-by: Cai Liu <liucai@xiaomi.com>

--- util.sh
+++ util.sh
@@ -4,9 +4,11 @@ reboot_device()
 	# reboot device
 	echo "  reboot device"
 	adb $adb_on_device shell reboot
-	sleep 60 # 1 min
+	sleep 60
 
 	adb $adb_on_device root
+	sleep 5
+
 	echo "  unlock screen"
 	adb $adb_on_device shell input keyevent 26 # press power
 	adb $adb_on_device shell input swipe 400 600 400 200 # swipe up to unlock screen

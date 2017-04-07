62ec0968131a43819a6dc22d599fa60189ccb2ba
Zhang Yuanchao <zhangyuanchao@xiaomi.com>
2017-04-07 10:27:45 +0800

    minor change for io_bench log
    Signed-off-by: Zhang Yuanchao <zhangyuanchao@xiaomi.com>

--- fio.sh
+++ fio.sh
@@ -21,6 +21,10 @@ run_io_benchmk()
 {
 	local cur_out_dir=$1
 	local loop_cnt=$2
+	if [ ! -e $cur_out_dir/io_bench.csv ]; then
+		echo -e "loop R-iops R-bw W-iops W-bw RR-iops RR-bw RW-iops RW-bw" \
+				> $cur_out_dir/io_bench.csv
+	fi
 	echo -n "  run_io_benchmk."
 	adb $adb_on_device shell my_fio "$fio_test_dir/$fio_config" &> .tmp/.fio_rw
 	if [ $? -eq 0 ]; then
@@ -28,7 +32,7 @@ run_io_benchmk()
 		local fio_save=`cat .tmp/.fio_rw | grep "IOPS=" | awk '{print $2" "$3}' | sed 's/IOPS=\(.*\), BW=\(.*\)[KkMm].*/\1 \2/g' | xargs`
 		echo -n "."
 		echo -e "[${GREEN}$fio_save${END}]"
-		echo "$loop_cnt $fio_save" >> $cur_out_dir/io_bench.txt 
+		echo "$loop_cnt $fio_save" >> $cur_out_dir/io_bench.csv
 	else
 		echo -n ".."
 		echo -e "[${RED}error${END}]"
